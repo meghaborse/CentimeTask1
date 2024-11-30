64c705bfa01264b9481ca7727a3d95da6b192ce9
Megha <megha.borse19@gmail.com>
2024-11-30 13:49:40 +0530

    service3 created

---
+++ service3/src/main/java/com/example/service3/controller/Service3Controller.java
@@ -0,0 +1,9 @@
+package com.example.service3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service3")
+public class Service3Controller {
+}
